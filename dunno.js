ff797c9e5b9232217a2863ac313ebf5796dfe33b
Anne-Lise VANHOEGAERDEN <al.vanhoegaerden@gmail.com>
2019-08-04 03:01:49 +0200

    refacto

--- dunno.js
+++ dunno.js
@@ -1,40 +1,29 @@
 $(document).ready(function() {
 
+      function randomNumber(){
+        return Math.floor(Math.random() * 175) + 1  
+      }
+
       $("#trigger").click(function() {
 
-
-        var hue = 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ')';
-        $("body").css("background-color",hue);
-        $("#socialNetworks a").css("background-color",hue);
-        $("#newQuote button").css("background-color",hue);
-        $("#quotation").css("color",hue);
-        $("#author p").css("color",hue);
-
-        // var cmbApi = "http://api.icndb.com/jokes/1"
-        //
-        // $.getJSON("quotations.js", function(json){
-        //     console.log(json.author);
-        // });
+        var randomColor = 'rgb(' + randomNumber() + ',' + randomNumber() + ',' + randomNumber() + ')';
+        $("body").css("background-color",randomColor);
+        $("#socialNetworks a").css("background-color",randomColor);
+        $("#newQuote button").css("background-color",randomColor);
+        $("#quotation").css("color",randomColor);
+        $("#author p").css("color",randomColor);
 
         $.ajax({
-            url: "http://api.icndb.com/jokes/",
-            dataType: 'json',
-            success: function(results){
-                var joke = results.value[Math.floor((Math.random() * 30) + 1)].joke;
-                var author = results.value.categories; ///??
-                $('.quotation').html(joke);
-                $('.author').html(categorie); /// ?
+          url: "http://api.icndb.com/jokes/",
+          dataType: 'json',
+          success: function(results){
+            var joke = results.value[Math.floor((Math.random() * 30) + 1)].joke;
+            $('.quotation').html(joke);
+            var jokeUrl = encodeURIComponent(joke)
+            $(".twitter-link").attr("href", `http://twitter.com/intent/tweet?text=${jokeUrl}`)
             }
         });
 
-
-      // $(quotation).html(json.quotations.author);
       });
+      $('#trigger').click();
  });
-
-//var quotations='{"quotations": { "quote": "Jeffrey", "author": "Giraffe"}, }';
-
-
-// var json=JSON.parse(quotations);
-// console.log(json);
-// console.log(json.quotations.author);
